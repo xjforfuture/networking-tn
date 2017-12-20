1e70d48d3c56f00282c690ee654d67ba61ef6e5e
xiongjun <xjforfuture@163.com>
2017-12-20 12:00:47 +0800

    add tmp

--- networking_tn/tools/migration.py
+++ networking_tn/tools/migration.py
@@ -103,10 +103,6 @@ class Fake_context(object):
                                           expire_on_commit=False)
         self.request_id = 'migration_context'
 
-class Fake_mech_context(object):
-    def __init__(self, **kwargs):
-        for key, value in kwargs.iteritems():
-            setattr(self, key, value)
 
 class Fake_TNL3ServicePlugin(l3_tn.TNL3ServicePlugin):
     def __init__(self):
@@ -199,11 +195,6 @@ class Fake_TNL3ServicePlugin(l3_tn.TNL3ServicePlugin):
                                 id=returned_obj['id'])
         return returned_obj
 
-def init_mech_driver():
-    mech_driver = mech_tn.FortinetMechanismDriver()
-    mech_driver.initialize()
-    return mech_driver
-
 def reset(dictionary):
     for key, value in dictionary.iteritems():
         if isinstance(value, list):
@@ -228,197 +219,7 @@ def cls2dict(record, dictionary, **kwargs):
             dictionary[kwargs[key]] = value
     return dictionary
 
-def network_migration(context, mech_driver):
-    """
-    # networks, ml2_network_segments
-    network =  {
-        #'status': 'ACTIVE',
-        #'subnets': [],
-        'name': u'test-net',
-        #'provider: physical_network': u'physnet1',
-        #'admin_state_up': True,
-        'tenant_id': u'11513667f4ee4a14acb0985659459988',
-        'provider: network_type': u'vlan',
-        'router:external': False,
-        #'shared': False,
-        'id': 'ff0a1d64-ce30-4ed0-ba37-597eaf8976f0',
-        #'provider: segmentation_id': 1200L
-    }
-    # ml2_network_segments
-    segments = [{
-        'segmentation_id': 1200L,
-        'physical_network': u'physnet1',
-        'id': u'e7dfa4fb-038a-4aad-b6fa-73afba788888',
-        'network_type': u'vlan'
-    }]
-    """
-    net =  {
-        'name': '',
-        'tenant_id': '',
-        'provider: network_type': '',
-        'router:external': False,
-        'id': '',
-    }
-
-    segment = {
-        'segmentation_id': 0,
-        'physical_network': '',
-        'id': '',
-        'network_type': ''
-    }
-    records = tn_db.query_records(context, models_v2.Network)
-    with Progress(len(records), 'network_migration') as p:
-        for record in records:
-            reset(net)
-            reset(segment)
-            db_seg = tn_db.query_record(context, ml2_db.NetworkSegment,
-                                              network_id=record.id)
-            cls2dict(record, net)
-            db_extnet = tn_db.query_record(context, ExternalNetwork,
-                                              network_id=record.id)
-            if db_extnet:
-                net['router:external'] = True
-
-            cls2dict(db_seg, segment)
-            net['provider: network_type'] = db_seg.network_type
-            mech_context = Fake_mech_context(_plugin_context=context,
-                                             current=net,
-                                             network_segments=[segment])
-            mech_driver.create_network_postcommit(mech_context)
-            p.update()
-
-
-def subnet_migration(context, mech_driver):
-    # table subnets
-    subnet =  {
-        'allocation_pools': [{
-            'start': '172.20.21.2',
-            'end': '172.20.21.254'
-        }],
-        'cidr': '172.20.21.0/24',
-        'id': 'ee1506dc-d1a9-45b3-840e-137bdaebce52',
-        'enable_dhcp': True,
-        'network_id': u'ad47f7b8-4bb7-4591-b8ed-f720237dd24f',
-        'tenant_id': u'11513667f4ee4a14acb0985659456f24',
-        'dns_nameservers': [],
-        'gateway_ip': u'172.20.21.1',
-        'shared': False
-    }
-    ipallocation_pool = {
-        'start': '172.20.21.2',
-        'end': '172.20.21.254'
-    }
-    records = tn_db.query_records(context, models_v2.Subnet)
-    with Progress(len(records), 'subnet_migration') as p:
-        for record in records:
-            dns_nameservers = []
-            reset(subnet)
-            reset(ipallocation_pool)
-            db_ipallocation = tn_db.query_record(context,
-                                                models_v2.IPAllocationPool,
-                                                subnet_id=record.id)
-            cls2dict(db_ipallocation, ipallocation_pool,
-                     first_ip='start', last_ip='end')
-            db_dnssrvs = tn_db.query_records(context,
-                                                   models_v2.DNSNameServer,
-                                                   subnet_id=record.id)
-
-            for dns in db_dnssrvs:
-                dns_nameservers.append(dns.address)
-            cls2dict(record, subnet)
-            subnet['dns_nameservers'] = dns_nameservers
-            subnet['allocation_pools'] = [ipallocation_pool]
-            mech_context = Fake_mech_context(_plugin_context=context,
-                                             current=subnet)
-            mech_driver.create_subnet_postcommit(mech_context)
-            p.update()
-
-def port_migration(context, mech_driver=None, l3_driver=None):
-    """
-    :param mech_driver:
-    :param context:
-    :return:
-    # table ports
-    port
-    {
-        'status': 'DOWN',
-        'binding: host_id': '',
-        'allowed_address_pairs': [],
-        'device_owner': 'network: router_interface',
-        'binding: profile': {
-
-        },
-        # table ipallocations
-        'fixed_ips': [{
-            'subnet_id': u'f645b09c-a34a-42fb-9c14-b999e43a54c7',
-            'ip_address': u'172.20.21.1'
-        }],
-        'id': 'fb66def6-bd5e-44a0-a3f7-7c0e8e08d9ff',
-        'security_groups': [],
-        'device_id': u'e4020c65-7003-468b-a34d-31af297397a0',
-        'name': '',
-        'admin_state_up': True,
-        'network_id': u'f8e34426-ccf7-429c-b726-3809d54cabdc',
-        'tenant_id': u'11513667f4ee4a14acb0985659456f24',
-        'binding: vif_details': {
-        },
-        'binding: vnic_type': 'normal',
-        'binding: vif_type': 'unbound',
-        'mac_address': u'00: 0c: 29: d9: 18: 3f'
-    }
-    """
-    port = {
-        'device_owner': 'network: router_interface',
-        'fixed_ips': [{
-            'subnet_id': u'f645b09c-a34a-42fb-9c14-b999e43a54c7',
-            'ip_address': u'172.20.21.1'
-        }],
-        'id': 'fb66def6-bd5e-44a0-a3f7-7c0e8e08d9ff',
-        'device_id': u'e4020c65-7003-468b-a34d-31af297397a0',
-        'admin_state_up': True,
-        'network_id': u'f8e34426-ccf7-429c-b726-3809d54cabdc',
-        'tenant_id': u'11513667f4ee4a14acb0985659456f24',
-        'mac_address': u'00: 0c: 29: d9: 18: 3f'
-    }
-    ipallocation = {
-        'subnet_id': u'f645b09c-a34a-42fb-9c14-b999e43a54c7',
-        'ip_address': u'172.20.21.1'
-    }
-    #MAC = utils.get_mac(mech_driver, context)
-    records = tn_db.query_records(context, models_v2.Port)
-
-    with Progress(len(records), 'port_migration') as p:
-         for record in records:
-            reset(port)
-            cls2dict(record, port)
-            if port['fixed_ips']:
-                fixed_ips = []
-                for fixed_ip in port['fixed_ips']:
-                    cls2dict(fixed_ip, ipallocation)
-                    fixed_ips.append(ipallocation)
-                port['fixed_ips'] = fixed_ips
-            if port['device_owner'] in [ROUTER_INTF, ROUTER_GW] and \
-               MAC not in port['mac_address']:
-                port['mac_address'] = MAC
-                if not tn_db.query_count(context, models_v2.Port,
-                    mac_address=MAC, network_id=record.network_id):
-                    tn_db.update_record(context, record,
-                                              mac_address=MAC)
-            mech_context = Fake_mech_context(_plugin_context=context,
-                                             current=port)
-            mech_driver.create_port_precommit(mech_context)
-            mech_driver.create_port_postcommit(mech_context)
-
-
-            db_routerport = tn_db.query_record(context,
-                                                     l3_models.RouterPort,
-                                                     port_id=record.id)
-            if getattr(db_routerport, 'port_type', None) in [ROUTER_INTF]:
-                l3_driver.add_router_interface(context, port)
-            p.update()
-
-
-def port_migration_new(context, l3_driver):
+def port_migration(context, l3_driver):
     """
     :param mech_driver:
     :param context:
@@ -531,7 +332,6 @@ def router_migration(context, l3_driver):
             l3_driver.create_router(context, router)
             p.update()
 
-
 def floatingip_migration(context, l3_driver):
     """
     # table floatingips, ipallocations
@@ -576,25 +376,12 @@ def floatingip_migration(context, l3_driver):
 
 def main():
     try:
-
-        print("step0")
         context = Fake_context()
-        print("step1")
-        #mech_driver = init_mech_driver()
-        print("step2")
         l3_driver = Fake_TNL3ServicePlugin()
-        print("step3")
         router_migration(context, l3_driver)
-        print("step4")
+        port_migration(context, l3_driver)
 
 
-        #network_migration(context, mech_driver)
-        print("step5")
-        #subnet_migration(context, mech_driver)
-        print("step6")
-
-
-        port_migration_new(context, l3_driver)
         '''
         print("step7")
         floatingip_migration(context, l3_driver)
